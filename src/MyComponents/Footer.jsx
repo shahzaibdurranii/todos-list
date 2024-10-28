9df58a4efe7036a7d6c31aa962f529735efb6402
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-10-28 10:32:39 +0500

    made the footer

--- src/MyComponents/Footer.jsx
+++ src/MyComponents/Footer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 const footer = () => {
+  const footerStyle = {
+    position: 'absolute',
+    top: '89vh',
+    width: '100%'
+  }
   return (
-    <div>Hey it works see!</div>
+    <footer className='bg-dark text-light py-3' style={footerStyle}>
+      <p className='text-center '>CopyRight &copy; MyTodosList.com</p>
+    </footer>
   )
 }
 
