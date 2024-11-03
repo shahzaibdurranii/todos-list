a2f0a98182236f9de0ae9c7ebe61615100a1727a
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-11-03 10:30:38 +0500

    added a new input field for date

--- src/MyComponents/AddTodo.jsx
+++ src/MyComponents/AddTodo.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { useState } from 'react'
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const AddTodo = (props) => {
 
   const [title,setTitle] = useState('');
   const [desc,setDesc] = useState('');
+  const [date, setDate] = useState('');
+
   const submit = (e)=>{
     e.preventDefault();
     if(!title || !desc){
       alert('Title or Description can not be blank!')
     }else{
-      props.addTodo(title , desc);
+      props.addTodo(title , desc , date);
       setTitle('');
-      setDesc('');  
+      setDesc(''); 
+      setDate('') ;
     }
     
   }  
@@ -22,16 +27,21 @@ const AddTodo = (props) => {
       <h3 className='text-center '>Add Todo</h3>
       <form onSubmit={submit}>
         <div className="mb-3">
-          <label HTMLFor="title" className="form-label">Todo Title</label>
+          <label htmlFor="title" className="form-label">Todo Title</label>
           <input type="text" className="form-control" value={title} onChange={(e)=>{setTitle(e.target.value)}} id="tittle" aria-describedby="emailHelp"/>
         </div>
         <div className="mb-3">
-          <label HTMLFor="desc" className="form-label">Todo Description</label>
+          <label htmlFor="desc" className="form-label">Todo Description</label>
           <input type="text" className="form-control" onChange={(e)=>{setDesc(e.target.value)}} value={desc} id="desc"/>
         </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-          <label className="form-check-label" HTMLFor="exampleCheck1">Check me out</label>
+        <div className="mb-3">
+          <label className="form-label">Select Date</label>
+          <input 
+          type='date'
+          className='form-control'
+          onChange={(e)=>{setDate(e.target.value)}} 
+          value={date} 
+          id='date'/>
         </div>
         <button type="submit" className="btn btn-success btn-sm">Add Todo</button>
       </form>
