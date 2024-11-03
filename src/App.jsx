d9c8db13baf108501fe33eb2b076cd52812dd4b4
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-11-03 10:30:05 +0500

    made routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,95 @@
-import './App.css';
-import Header from './MyComponents/Header';
-import Todos from './MyComponents/Todos';
-import Footer from './MyComponents/Footer';
-import { useState } from 'react';
-import AddTodo from './MyComponents/AddTodo';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Header from "./MyComponents/Header";
+import Todos from "./MyComponents/Todos";
+import Footer from "./MyComponents/Footer";
+import { useState, useEffect } from "react";
+import AddTodo from "./MyComponents/AddTodo";
+import About from './MyComponents/About';
+import backgroundImage from './images/blue web bg.jpeg';
+
 
 function App() {
-  const onDelete = (todo) =>{
-    console.log('I am onDelete of todo!',todo);
-    setTodos(todos.filter((e)=>{
-      return e!==todo;
-    }))
+
+  const appStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundAttachment: "fixed",
+    minHeight: "100vh",
+  } 
+
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  } else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
   }
 
-  const addTodo = (title , desc)=>{
-    console.log('i am adding this todo',title,desc);
+  const onDelete = (todo) => {
+    console.log("I am onDelete of todo!", todo);
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+    localStorage.getItem("todos", JSON.stringify(todos));
+  };
+
+  const addTodo = (title, desc , date) => {
+    console.log("i am adding this todo", title, desc);
     let sno;
-    if(todos.length == 0){
+    if (todos.length === 0) {
       sno = 0;
-    }else{
-      sno = todos[todos.length-1].sno+1;
+    } else {
+      sno = todos[todos.length - 1].sno + 1;
     }
     const myTodo = {
-      sno:sno,
-      title : title,
-      desc :desc
-    }
-    setTodos([...todos , myTodo])
-    console.log(myTodo)
-  }
-
-  const [todos , setTodos] = useState([]);
-
-  const containerStyle = {
-    flex: 1, // Ensures main content takes available space
+      sno: sno,
+      title: title,
+      desc: desc,
+      date : date
+    };
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
   };
+
+  const [todos, setTodos] = useState(initTodo);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  
+
 
   return (
     <>
-      <Header title='Title' searchBar={true}/>
-      <div className='container'>
-          <AddTodo addTodo={addTodo}/>
-          <Todos todos={todos} onDelete={onDelete}/>
+    <Router>
+      <div style={appStyle} className="app-container">
+        <div className='header-space'>
+          <Header title="Title" searchBar={true} />
+        </div>
+        <div className="container blur-background my-8">
+          <Routes>
+            <Route path="/" element={
+              <>
+                <AddTodo addTodo={addTodo} />
+                <Todos todos={todos} onDelete={onDelete} />
+              </>
+            } />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <div className='footer-space'>
+          <Footer />
+        </div>
       </div>
-      <Footer/>
-    </>
+    </Router>
+   </>   
     
+      
   );
 }
 
