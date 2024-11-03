ac04cc8b8a56a8683973eb6dc687aac8af52c5fb
shahzaibdurranii <durranishahzaib0@gmail.com>
2024-11-03 10:31:04 +0500

    displayed date within the todo item

--- src/MyComponents/TodoItem.jsx
+++ src/MyComponents/TodoItem.jsx
@@ -5,6 +5,7 @@ const TodoItem = ({todo , onDelete}) => {
     <div>
       <h4>{todo.title}</h4>
       <p>{todo.desc}</p>
+      <p ><strong>Date:</strong> {todo.date}</p> 
       <button className="btn btn-sm btn-danger" onClick={()=>{onDelete(todo)}}>Delete</button>
     </div>
   )
